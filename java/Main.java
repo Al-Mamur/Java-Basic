c741617312b009a5f7053b152a5d001645394ef8
Sayed Mamur <68020674+Al-Mamur@users.noreply.github.com>
2022-05-19 23:39:10 +0600

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public void ferarri(){
+        System.out.println("Worlds best car");
+    }
+    public void speed(int x){
+        System.out.println("maxSpeed = "+ x);
+    }
+    public void color(){
+        System.out.println("This is a red car");
+    }
+    public void categori(){
+        System.out.println("Sports_Car");
+    }
+}
---
+++ java/Second.java
@@ -0,0 +1,10 @@
+public class Second {
+    public static void main(String[] args){
+        Main mycar = new Main();
+        mycar.ferarri();
+        mycar.speed(200);
+        mycar.categori();
+        mycar.color();
+       
+    }
+}
